fc7fc62bacc6b68d253803b92a841d25df3e5757
jjyunho <cyh@gist.ac.kr>
2024-10-22 18:21:29 +0900

    update imu read code

--- walker.py
+++ walker.py
@@ -56,11 +56,11 @@ class Walker:
     def _run_imu(self):
         arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
         while not self.stop_event.is_set():
-            arduino.write('7')
+            arduino.write('7'.encode('utf-8'))
             print('7'.encode('utf-8'))
             time.sleep(1)
-            self.tilt = arduino.readline()
-            print(self.tilt.decode('utf-8'))
+            self.tilt = arduino.readline().decode('utf-8')
+            print(self.tilt)
         pass
 
     def _run_dc(self):
