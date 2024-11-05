5c55fcbd9ba1acc293639a509701bf6e61966a39
jjyunho <cyh@gist.ac.kr>
2024-11-05 18:11:39 +0900

    dc motoer code update

--- parts/step_motor_controller.py
+++ parts/step_motor_controller.py
@@ -43,10 +43,10 @@ class StepMotorControl:
 	def update(self, direction, pulses):
 		if direction == 'high':
 			GPIO.output(self.dir_pin_lf, GPIO.HIGH)
-			GPIO.output(self.dir_pin_rf, GPIO.HIGH)# 방향 설정 (HIGH 또는 LOW)
+			# GPIO.output(self.dir_pin_rf, GPIO.HIGH)# 방향 설정 (HIGH 또는 LOW)
 		else:
 			GPIO.output(self.dir_pin_lf, GPIO.LOW)
-			GPIO.output(self.dir_pin_rf, GPIO.LOW)
+			# GPIO.output(self.dir_pin_rf, GPIO.LOW)
 
 		for i in range(pulses):
 			GPIO.output(self.pul_pin_rf, GPIO.HIGH)
@@ -56,13 +56,13 @@ class StepMotorControl:
 			time.sleep(0.0005)
 			# time.sleep(1)
 
-		for i in range(pulses):
-			GPIO.output(self.pul_pin_lf, GPIO.HIGH)
-			time.sleep(0.0005)  # 펄스 너비
-			# time.sleep(1)
-			GPIO.output(self.pul_pin_lf, GPIO.LOW)
-			time.sleep(0.0005)  # 펄스 간격
-			# time.sleep(1)
+		# for i in range(pulses):
+		# 	GPIO.output(self.pul_pin_lf, GPIO.HIGH)
+		# 	time.sleep(0.0005)  # 펄스 너비
+		# 	# time.sleep(1)
+		# 	GPIO.output(self.pul_pin_lf, GPIO.LOW)
+		# 	time.sleep(0.0005)  # 펄스 간격
+		# 	# time.sleep(1)
 
 	def upward(self, angle):
 		self.update('high', 500)
