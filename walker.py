ae4407c1e58aff81efdd401d497c60d66c0214a8
jjyunho <cyh@gist.ac.kr>
2024-11-05 19:01:48 +0900

    dc motoer code update

--- walker.py
+++ walker.py
@@ -80,8 +80,8 @@ class Walker:
             #     self.step_motor.upward(self.tilt)
             # elif self.tilt < -45:
             #     self.step_motor.downward(self.tilt)
-            self.step_motor.upward(self.tilt)
-            time.sleep(0.2)
+            # self.step_motor.upward(self.tilt)
+            # time.sleep(0.2)
             self.step_motor.downward(self.tilt)
             time.sleep(0.2)
             test = 'test'
