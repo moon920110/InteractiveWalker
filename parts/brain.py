b4a4e97858ad5c38090ce0990c4975222b1f6bb2
jjyunho <cyh@gist.ac.kr>
2024-12-12 14:12:19 +0900

    brain code update

--- parts/brain.py
+++ parts/brain.py
@@ -63,8 +63,8 @@ class Brain:
 		visual_image = cv2.resize(visual_image.astype(np.uint8), (500, 500))
 
 		cv2.imshow("Pressure", visual_image)
-		if cv2.waitKey(1) & 0xff == 27:
-			break
+		# if cv2.waitKey(1) & 0xff == 27:
+		# 	break
 		# _, angle, speed = self.model(images, hmd_yaw=0)
 		angle = 0
 		speed = 0
