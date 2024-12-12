781a1ad85ebd52ac0147b28ace02fc4d5696327f
jjyunho <cyh@gist.ac.kr>
2024-12-12 13:41:52 +0900

    brain code update

--- walker.py
+++ walker.py
@@ -48,7 +48,7 @@ class Walker:
         arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
         while not self.stop_event.is_set():
             arduino.write('7'.encode('utf-8'))
-            # self.angle, self.speed = self.brain.think()
+            self.angle, self.speed = self.brain.think()
             #TODO
             test = 'test'
 
