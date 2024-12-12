cf43e62881cdcb6d852c6e41813a0b103fb4ce9d
jjyunho <cyh@gist.ac.kr>
2024-12-12 17:13:26 +0900

    brain code update

--- parts/brain.py
+++ parts/brain.py
@@ -49,12 +49,12 @@ class Brain:
 	def think(self):
 		if self.start_signal == 1:
 			self.start_signal = 0
-			for i in range(200):
+			for i in range(50):
 				total_image = self.sensor.get()
 				self.base_images.append(total_image[-1])
 			base_images = np.array(self.base_images[:-30])
 			self.base_image = np.mean(base_images, axis=0)
-			_, right_arm_cnts, _ = cv2.findContours(self.base_image[22:,:], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+			_, right_arm_cnts, _ = cv2.findContours(self.base_image[22:,:].astype(np.int), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 			sorted_right_arm_cnts = sorted(right_arm_cnts, key=cv2.contourArea, reverse=True)
 			M = cv2.moments(sorted_right_arm_cnts)
 			right_arm_cx = int(M['m10'] / M['m00'])
