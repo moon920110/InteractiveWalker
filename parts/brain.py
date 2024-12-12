62d7f17414b89c814db49a9e332bc22badba88d2
jjyunho <cyh@gist.ac.kr>
2024-12-13 00:32:42 +0900

    brain code update

--- parts/brain.py
+++ parts/brain.py
@@ -57,7 +57,7 @@ class Brain:
 			# temp = (self.base_image[21:,:]/100).astype(np.uint8).reshape(1, 11, 32)
 			# temp = cv2.adaptiveThreshold(temp, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
 			temp = np.array([self.base_image[21:,:]/100, self.base_image[21:,:]/100, self.base_image[21:,:]/100], dtype='uint8')
-			_, right_arm_cnts, _ = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+			right_arm_cnts, _ = cv2.findContours(temp, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 			sorted_right_arm_cnts = sorted(right_arm_cnts, key=cv2.contourArea, reverse=True)
 			M = cv2.moments(sorted_right_arm_cnts)
 			right_arm_cx = int(M['m10'] / M['m00'])
