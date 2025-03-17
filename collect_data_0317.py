d2ff7019be84e9a3d043626b14a55118a881a9a2
jjyunho <cyh@gist.ac.kr>
2025-03-17 20:50:05 +0900

    data collecting code

--- collect_data_0317.py
+++ collect_data_0317.py
@@ -73,7 +73,7 @@ def main(
 
         #imu
         arduino.write('imu'.encode('utf-8'))
-        time.sleep(0.03)
+        sleep(0.03)
         imu = float(arduino.readline().decode('utf-8').strip())
         print(imu)
 
