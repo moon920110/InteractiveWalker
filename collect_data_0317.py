5628bbbf138dcd8265037a094f98f9dd7db2503b
jjyunho <cyh@gist.ac.kr>
2025-03-17 19:46:38 +0900

    data collecting code

--- collect_data_0317.py
+++ collect_data_0317.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
     main(
         max_frame = max_frame,
-        foldername = f'/home/cilab/media/yhssd/walker_data1024/{name}',
+        foldername = f'/data_collect/{name}',
         filename = label,
         normalize = False
     )
