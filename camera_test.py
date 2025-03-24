d0ce6a6b3f3a6ac0fb2a63bb6019b64934b77dc1
jjyunho <cyh@gist.ac.kr>
2025-03-24 19:25:32 +0900

    camera test update

--- camera_test.py
+++ camera_test.py
@@ -15,11 +15,23 @@ intrinsics = depth_stream.as_video_stream_profile().get_intrinsics()
 # Depth scaling factor
 depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
 
-def detect_obstacles(depth_frame, threshold=1.5):
+def detect_obstacle_left(depth_frame, threshold=1.5):
     """Detects obstacles closer than a given threshold (meters)"""
-    depth_image = np.asanyarray(depth_frame.get_data()) * depth_scale  # Convert depth to meters
+    depth_image = np.asanyarray(depth_frame.get_data())[200:280, 170:270] * depth_scale  # Convert depth to meters
     mask = (depth_image > 0) & (depth_image < threshold)  # Highlight obstacles closer than threshold
     return mask, depth_image
+def detect_obstacle_mid(depth_frame, threshold=1.5):
+    """Detects obstacles closer than a given threshold (meters)"""
+    depth_image = np.asanyarray(depth_frame.get_data())[200:280, 270:370] * depth_scale  # Convert depth to meters
+    mask = (depth_image > 0) & (depth_image < threshold)  # Highlight obstacles closer than threshold
+    return mask, depth_image
+
+def detect_obstacle_right(depth_frame, threshold=1.5):
+    """Detects obstacles closer than a given threshold (meters)"""
+    depth_image = np.asanyarray(depth_frame.get_data())[200:280, 370:470] * depth_scale  # Convert depth to meters
+    mask = (depth_image > 0) & (depth_image < threshold)  # Highlight obstacles closer than threshold
+    return mask, depth_image
+
 
 def compute_distance(depth_image, mask):
     """Compute average distance of obstacles from the camera"""
@@ -39,12 +51,12 @@ try:
             continue
 
         # Detect obstacles within 1.5 meters
-        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 170:270], threshold=1.0)
+        obstacle_mask, depth_image = detect_obstacle_left(depth_frame, threshold=1.0)
         # Compute the average distance of obstacles
         left_average_distance = compute_distance(depth_image, obstacle_mask)
-        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 270:370], threshold=1.0)
+        obstacle_mask, depth_image = detect_obstacle_mid(depth_frame, threshold=1.0)
         mid_average_distance = compute_distance(depth_image, obstacle_mask)
-        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 370:470], threshold=1.0)
+        obstacle_mask, depth_image = detect_obstacle_right(depth_frame, threshold=1.0)
         right_average_distance = compute_distance(depth_image, obstacle_mask)
 
         # Normalize depth image for visualization
