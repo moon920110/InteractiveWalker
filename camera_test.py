ad77f8fe44c39d6b05559e048b2cf556667c4247
jjyunho <cyh@gist.ac.kr>
2025-03-24 19:20:31 +0900

    camera test update

--- camera_test.py
+++ camera_test.py
@@ -15,7 +15,7 @@ intrinsics = depth_stream.as_video_stream_profile().get_intrinsics()
 # Depth scaling factor
 depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
 
-def detect_obstacles(depth_frame, threshold=3.0):
+def detect_obstacles(depth_frame, threshold=1.5):
     """Detects obstacles closer than a given threshold (meters)"""
     depth_image = np.asanyarray(depth_frame.get_data()) * depth_scale  # Convert depth to meters
     mask = (depth_image > 0) & (depth_image < threshold)  # Highlight obstacles closer than threshold
@@ -38,11 +38,14 @@ try:
         if not depth_frame:
             continue
 
-        # Detect obstacles within 3 meters
-        obstacle_mask, depth_image = detect_obstacles(depth_frame, threshold=1.0)
-
+        # Detect obstacles within 1.5 meters
+        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 170:270], threshold=1.0)
         # Compute the average distance of obstacles
-        average_distance = compute_distance(depth_image, obstacle_mask)
+        left_average_distance = compute_distance(depth_image, obstacle_mask)
+        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 270:370], threshold=1.0)
+        mid_average_distance = compute_distance(depth_image, obstacle_mask)
+        obstacle_mask, depth_image = detect_obstacles(depth_frame[200:280, 370:470], threshold=1.0)
+        right_average_distance = compute_distance(depth_image, obstacle_mask)
 
         # Normalize depth image for visualization
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=50), cv2.COLORMAP_JET)
@@ -54,10 +57,14 @@ try:
         cv2.imshow("Depth Map with Obstacles within 3m", depth_colormap)
 
         # Display the average distance of obstacles within 3 meters
-        if average_distance is not None:
-            print(f"Average distance of obstacles within 3 meters: {average_distance:.2f} meters")
-        else:
-            print("No obstacles within 3 meters detected.")
+        if left_average_distance is not None:
+            print(f"Average distance of obstacles within 1.5 meters: {left_average_distance:.2f} meters")
+        if mid_average_distance is not None:
+            print(f"Average distance of obstacles within 1.5 meters: {mid_average_distance:.2f} meters")
+        if right_average_distance is not None:
+            print(f"Average distance of obstacles within 1.5 meters: {right_average_distance:.2f} meters")
+        # else:
+        #     print("No obstacles within 1.5 meters detected.")
 
         # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
