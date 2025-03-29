94bffb94e6c40e360f7bc54ecf86a012fd5944a5
jjyunho <cyh@gist.ac.kr>
2025-03-29 17:24:39 +0900

    camera test update

--- walker.py
+++ walker.py
@@ -245,10 +245,10 @@ class Walker:
             if mode == 'full':
                 brain_thread.start()
                 self.logger.info(f'[Walker] Brain thread start')
-                camera_thread.start()
-                self.logger.info(f'[Walker] camera thread start')
+                # camera_thread.start()
+                # self.logger.info(f'[Walker] camera thread start')
                 brain_thread.join()
-                camera_thread.join()
+                # camera_thread.join()
             keyinput_thread.join()
             imu_thread.join()
 
