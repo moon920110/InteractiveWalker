309d88e2dfc080635cc25ddfb52ac0833fc0a669
jjyunho <cyh@gist.ac.kr>
2025-03-29 20:49:39 +0900

    camera test update

--- walker.py
+++ walker.py
@@ -79,7 +79,8 @@ class Walker:
 
     def init(self, args):
         arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
-        mode = args.mode
+        time.sleep(5)
+        mode = args.mode + '\n'
         arduino.write(mode.encode('utf-8'))
         time.sleep(5)
         print('mode set')
