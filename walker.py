b478d5c463f035f1f8abb1305eafd74da8faf744
jjyunho <cyh@gist.ac.kr>
2025-04-02 22:01:54 +0900

    python communication test

--- walker.py
+++ walker.py
@@ -134,7 +134,7 @@ class Walker:
                     arduino.write(command.encode('utf-8'))
                     time.sleep(0.1)
                 else:
-                    command = 'S1 ' + str(int(13 * self.forback * self.left_turn_scale)) + ',S2 ' + str(int(13 * self.forback * self.right_turn_scale)) + ",D1 0,D2 0,"
+                    command = 'S1 ' + str(int(8 * self.forback * self.left_turn_scale)) + ',S2 ' + str(int(8 * self.forback * self.right_turn_scale)) + ",D1 0,D2 0,"
                     # arduino.write('S1 0,S2 0,D1 0,D2 0'.encode('utf-8'))
                     arduino.write(command.encode('utf-8'))
                     # print(self.angle, self.speed, 'write')
@@ -256,7 +256,7 @@ class Walker:
         imu_thread = threading.Thread(target=self._run_imu, args=(keyQueue, mode))
         # if mode == 'full':
         brain_thread = threading.Thread(target=self._run_brain)
-        # camera_thread = threading.Thread(target=self._run_camera)
+        camera_thread = threading.Thread(target=self._run_camera)
         # keyinput_thread = threading.Thread(target=self._run_keyinput, args=(keyQueue,))
 
         try:
@@ -267,10 +267,10 @@ class Walker:
             # if mode == 'full':
             brain_thread.start()
             self.logger.info(f'[Walker] Brain thread start')
-            # camera_thread.start()
-            # self.logger.info(f'[Walker] camera thread start')
+            camera_thread.start()
+            self.logger.info(f'[Walker] camera thread start')
             brain_thread.join()
-            # camera_thread.join()
+            camera_thread.join()
             # keyinput_thread.join()
             imu_thread.join()
 
