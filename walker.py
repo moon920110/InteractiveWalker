1253b82776f178c972be99805b087bd2ef2c9d99
jjyunho <cyh@gist.ac.kr>
2025-04-05 15:55:45 +0900

    update brain code

--- walker.py
+++ walker.py
@@ -149,7 +149,7 @@ class Walker:
     def _run_brain(self):
         while not self.stop_event.is_set():
             self.forback, self.leftright, self.STS = self.brain.think()
-            # print(self.forback)
+            print('forback: ', self.forback, ' leftright: ', self.leftright)
             if self.leftright <= -0.25 or self.leftright >= 0.25:
                 self.forback = 0
                 self.leftright_flag = True
