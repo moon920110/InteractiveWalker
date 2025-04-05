7514a5b59ce7f0485d9edd2d4a7d3e353987cc1a
jjyunho <cyh@gist.ac.kr>
2025-04-05 18:09:10 +0900

    update brain code

--- walker.py
+++ walker.py
@@ -139,7 +139,7 @@ class Walker:
                     arduino.write(command.encode('utf-8'))
                     time.sleep(0.1)
                 else:
-                    command = 'S1 ' + str(int(20 * self.forback * self.left_turn_scale)) + ',S2 ' + str(int(20 * self.forback * self.right_turn_scale)) + ",D1 0,D2 0,"
+                    command = 'S1 ' + str(min(int(20 * self.forback * self.left_turn_scale), 200)) + ',S2 ' + str(min(int(20 * self.forback * self.right_turn_scale), 200)) + ",D1 0,D2 0,"
                     # arduino.write('S1 0,S2 0,D1 0,D2 0'.encode('utf-8'))
                     if command != self.precommand:
                         arduino.write(command.encode('utf-8'))
