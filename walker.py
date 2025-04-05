de1b5b39f2812acc2304a359b877e54905bab851
jjyunho <cyh@gist.ac.kr>
2025-04-05 13:33:36 +0900

    update brain code

--- walker.py
+++ walker.py
@@ -206,14 +206,14 @@ class Walker:
                 continue
 
             # Detect obstacles within 1.5 meters
-            obstacle_mask, depth_image = detect_obstacle_left(depth_frame, depth_scale, threshold=1.0)
+            obstacle_mask, depth_image = detect_obstacle_left(depth_frame, depth_scale, threshold=1.5)
             # Compute the average distance of obstacles
             left_average_distance = compute_distance(depth_image, obstacle_mask)
-            obstacle_mask, depth_image = detect_obstacle_mid(depth_frame, depth_scale, threshold=1.0)
+            obstacle_mask, depth_image = detect_obstacle_mid(depth_frame, depth_scale, threshold=1.5)
             mid_average_distance = compute_distance(depth_image, obstacle_mask)
-            obstacle_mask, depth_image = detect_obstacle_right(depth_frame, depth_scale, threshold=1.0)
+            obstacle_mask, depth_image = detect_obstacle_right(depth_frame, depth_scale, threshold=1.5)
             right_average_distance = compute_distance(depth_image, obstacle_mask)
-            obstacle_mask, depth_image = detect_obstacles(depth_frame, depth_scale, threshold=1.0)
+            obstacle_mask, depth_image = detect_obstacles(depth_frame, depth_scale, threshold=1.5)
 
             # Normalize depth image for visualization
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=50), cv2.COLORMAP_JET)
