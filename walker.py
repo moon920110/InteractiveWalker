a5a5278765dd0de3ba10cb9d6a9676d0b777a19a
jjyunho <cyh@gist.ac.kr>
2025-08-20 21:30:58 +0900

    new tactile sensor replace

--- walker.py
+++ walker.py
@@ -165,7 +165,7 @@ class Walker:
     def _run_brain(self):
         while not self.stop_event.is_set():
             print('step1')
-            print(q)
+            print(q.get())
             self.forback, self.leftright, self.STS = self.brain.think(q)
             print('step2')
             # print('forback: ', self.forback, ' leftright: ', self.leftright)
