2b078d49f6adf94a61c7a92fe459cf4d245d168e
jjyunho <cyh@gist.ac.kr>
2025-09-12 14:57:06 +0900

    sensitivity change test

--- collect_data_0317.py
+++ collect_data_0317.py
@@ -47,6 +47,7 @@ def main(
         #     print(total_image.shape)
 
         print(storage.frameCount)
+        print(stage.empty())
         if storage.frameCount == 50:
             stage.put('collect')
             print('tactile signal mode changed to collect')
